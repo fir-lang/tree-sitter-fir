6a75a9c491db1e4123fcf77f9a528de8b260c2c6
Ömer Sinan Ağacan <omeragacan@gmail.com>
2026-02-25 09:24:19 +0000

    Add PASS/FAIL prefix to tested file paths

--- test.sh
+++ test.sh
@@ -7,11 +7,16 @@ FILE="${1:-../fir/tests}"
 pass=0
 fail=0
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+RESET='\033[0m'
+
 for f in $FILE/**/*.fir; do
-    echo $f
     if timeout 5 tree-sitter parse "$f" > /dev/null 2>&1; then
+        echo -e "${GREEN}PASS${RESET} $f"
         pass=$((pass + 1))
     else
+        echo -e "${RED}FAIL${RESET} $f"
         fail=$((fail + 1))
     fi
 done
